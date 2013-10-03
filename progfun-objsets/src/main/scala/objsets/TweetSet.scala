3341798db9663d2f3e4b54498bd1007b5e83e907
Frederic Jacobs <github@fredericjacobs.com>
2013-10-03 11:46:06 +0200

    Ex 1-2

--- progfun-objsets/src/main/scala/objsets/TweetSet.scala
+++ progfun-objsets/src/main/scala/objsets/TweetSet.scala
@@ -39,34 +39,41 @@ abstract class TweetSet {
    * This method takes a predicate and returns a subset of all the elements
    * in the original set for which the predicate is true.
    *
-   * Question: Can we implment this method here, or should it remain abstract
+   * Question: Can we implement this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-  def filter(p: Tweet => Boolean): TweetSet = ???
-
+  
+  // Answer: The filter method can be implemented here since all it does is call its helper method in both cases (Empty or NonEmpty)
+  
+  def filter(p: Tweet => Boolean): TweetSet = this.filterAcc(p, new Empty)
+  
   /**
-   * This is a helper method for `filter` that propagetes the accumulated tweets.
+   * This is a helper method for `filter` that propagates the accumulated tweets.
    */
   def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet
-
+  
   /**
    * Returns a new `TweetSet` that is the union of `TweetSet`s `this` and `that`.
    *
-   * Question: Should we implment this method here, or should it remain abstract
+   * Question: Should we implement this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-   def union(that: TweetSet): TweetSet = ???
+   def union(that: TweetSet): TweetSet
 
+   // Answer: We implement it in the subclasses. If we have an empty set, "that" is the union. If not, we need additional parameters to go into the binary tree.
+   
   /**
    * Returns the tweet from this set which has the greatest retweet count.
    *
    * Calling `mostRetweeted` on an empty set should throw an exception of
    * type `java.util.NoSuchElementException`.
    *
-   * Question: Should we implment this method here, or should it remain abstract
+   * Question: Should we implement this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-  def mostRetweeted: Tweet = ???
+  def mostRetweeted: Tweet
+  
+  // Answer: This should be implemented in subclasses. 
 
   /**
    * Returns a list containing all tweets of this set, sorted by retweet count
@@ -74,7 +81,7 @@ abstract class TweetSet {
    * have the highest retweet count.
    *
    * Hint: the method `remove` on TweetSet will be very useful.
-   * Question: Should we implment this method here, or should it remain abstract
+   * Question: Should we implement this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
   def descendingByRetweet: TweetList = ???
@@ -110,13 +117,16 @@ abstract class TweetSet {
 
 class Empty extends TweetSet {
 
-  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-
+  override def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
 
   /**
    * The following methods are already implemented
    */
+  
+  override def union(that: TweetSet): TweetSet = that
 
+  override def mostRetweeted: Tweet = throw new java.util.NoSuchElementException;
+  
   def contains(tweet: Tweet): Boolean = false
 
   def incl(tweet: Tweet): TweetSet = new NonEmpty(tweet, new Empty, new Empty)
@@ -128,9 +138,32 @@ class Empty extends TweetSet {
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+  override def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = {
+		if(p(elem))
+		  left.filterAcc(p, right.filterAcc(p, acc.incl(elem)))
+		else 
+		  left.filterAcc(p, right.filterAcc(p, acc))
+  }
+  
+  override def union(that: TweetSet): TweetSet = {
+		(left union (right union that)).incl(elem)
+	}
 
+  override def mostRetweeted: Tweet = {
+	lazy val leftMost = left.mostRetweeted
+	lazy val rightMost = right.mostRetweeted
 
+		if( !left.empty && leftMost.retweets > elem.retweets )
+			if( !right.empty && rightMost.retweets > leftMost.retweets )
+				rightMost
+			else
+				leftMost
+		else if( !right.empty && rightMost.retweets > elem.retweets )
+			rightMost
+		else
+			elem
+	}
+  
   /**
    * The following methods are already implemented
    */
